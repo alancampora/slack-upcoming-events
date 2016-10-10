38af21a087ddd3399dd3260b23952ab8492e171b
Alan Campora <alan.campora@gmail.com>
2016-10-09 22:23:17 -0300

    [ADD] new formatter for text output

--- index.js
+++ index.js
@@ -9,14 +9,16 @@ var oScheduler = new Scheduler();
 var oSlack = new Slack();
 
 // Schedule event to check if there is any anniversary between running date (today) and next week (7 days gap) 
-var oEvent = new Event(7,new Date(),data);
+var oEvent = new Event(7,'09/07/2016',data);
 
 oScheduler.scheduleEvent({
-    runningDay:0,
-    iternval: 1000,
-    runningHour:20,
+    runningDay:0, 
+    iternval: 1000, 
+    runningHour:21,
     successCallback:function(){
         var events = oEvent.upcomingEvents();
+
+        var text = oFomatter.formatEvents(events);
         
         oSlack.say(
             'THIS IS A MESSAGE', 
@@ -24,6 +26,6 @@ oScheduler.scheduleEvent({
                 console.log('success'); 
             })       
     }
-);
+});
 
 
