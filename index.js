fc0f037c225a80abf491b471a944685df521a423
Alan Campora <alan.campora@gmail.com>
2016-10-09 21:15:58 -0300

    [MOD] changing parameter for scheduler

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ var Scheduler = require('./lib/Scheduler');
 var Slack = require('./lib/Slack');
 
 
-var oEvent = new Event(7,new Date(),data);
 var oScheduler = new Scheduler();
 var oSlack = new Slack();
 
-oScheduler.scheduleEvent(oEvent,0,1000,20,
-    function(){
+// Schedule event to check if there is any anniversary between running date (today) and next week (7 days gap) 
+var oEvent = new Event(7,new Date(),data);
+
+oScheduler.scheduleEvent({
+    runningDay:0,
+    iternval: 1000,
+    runningHour:20,
+    successCallback:function(){
         var events = oEvent.upcomingEvents();
+        
         oSlack.say(
             'THIS IS A MESSAGE', 
             function(){
