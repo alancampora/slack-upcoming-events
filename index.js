1cf92d9a2ee2510959aa2750acf7fb18c2bfaf0d
Alan Campora <alan.campora@gmail.com>
2016-10-10 02:42:42 -0300

    [MOD] changing intervals

--- index.js
+++ index.js
@@ -3,25 +3,25 @@ var data = require('./lib/test/dummyData.json');
 var Event = require('./lib/Event');
 var Scheduler = require('./lib/Scheduler');
 var Slack = require('./lib/Slack');
-
+var Formatter = require('./lib/Formatter');
 
 var oScheduler = new Scheduler();
 var oSlack = new Slack();
-
+var oFormatter = new Formatter();
 // Schedule event to check if there is any anniversary between running date (today) and next week (7 days gap) 
 var oEvent = new Event(7,'09/07/2016',data);
 
 oScheduler.scheduleEvent({
     runningDay:0, 
-    iternval: 1000, 
-    runningHour:21,
+    interval: 60*1000, 
+    runningHour:22,
     successCallback:function(){
         var events = oEvent.upcomingEvents();
 
-        var text = oFomatter.formatEvents(events);
+        var text = oFormatter.eventsToStr(events);
         
         oSlack.say(
-            'THIS IS A MESSAGE', 
+            text, 
             function(){
                 console.log('success'); 
             })       
